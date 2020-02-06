676efff7b165de483ef7eada37bebf43fc0f44e6
minhradz <minhradz@gmail.com>
2020-02-06 16:28:03 +0000

    googleAuth2.0 running

--- index.js
+++ index.js
@@ -3,9 +3,28 @@ const express = require('express')
 const passport =require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express()
+const keys = require('./config/keys')
 
 // console.developers.google.com
-passport.use(new GoogleStrategy())
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, 
+        accessToken => {
+            console.log(accessToken)
+        }
+    )
+)
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+)
 
 // PORT for deployment environment, 500 for development deployment( local )
 app.listen(process.env.PORT || 5000)
